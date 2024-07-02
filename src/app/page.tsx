c0b5665db9cf1edcd120c35aad78c603926c00a7
karangoraniya <karangoraniya@gmail.com>
2024-07-02 19:19:31 +0530

    feat : Internet Identity added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,142 @@
+"use client"
 import Image from "next/image";
+import React, { useEffect, useState } from 'react'
+import { AuthClient } from "@dfinity/auth-client"
+import { Actor, HttpAgent, Identity } from '@dfinity/agent';
+import { idlFactory } from '@/utils/blockbolt.did'
+import { _SERVICE as MyService } from '@/utils/blockbolt.did'; // Adjust the path to your did.js
+import { Principal } from '@dfinity/principal';
+import { Button } from "@/components/ui/button"
 
+
+
+const blockBoltcanisterId = "bza44-ciaaa-aaaan-qlvna-cai";
+const nnsCanisterId = 'mxzaz-hqaaa-aaaar-qaada-cai';
+const whitelist = [nnsCanisterId, blockBoltcanisterId];
 export default function Home() {
+
+  const [greeting, setGreeting] = useState<string>("");
+  const [actor, setActor] = useState<any>(null);
+  const [authClient, setAuthClient] = useState<AuthClient | null>(null);
+  const [identiy, setIdentiy] = useState<Identity | null>(null);
+  const [addressData, setAddressData] = useState<any>(null);
+  const [errorMessage, setErrorMessage] = useState("")
+
+  useEffect(() => {
+
+    const initAuth = async () => {
+      const client = await AuthClient.create();
+      setAuthClient(client);
+
+      if (await client.isAuthenticated()) {
+        const identity = await client.getIdentity();
+        setIdentiy(identity);
+        const agent = new HttpAgent({ identity: identity });
+        const actor = Actor.createActor(idlFactory, { agent, canisterId: blockBoltcanisterId });
+        setActor(actor);
+      }
+    };
+
+    initAuth();
+  }, []);
+
+  const login = async () => {
+    if (authClient) {
+      await authClient.login({
+        maxTimeToLive: BigInt(7 * 24 * 60 * 60 * 1000 * 1000 * 1000),
+        onSuccess: async () => {
+          const identity = await authClient.getIdentity();
+          setIdentiy(identity);
+          console.log(identity.getPrincipal().toString());
+
+          const agent = new HttpAgent({ identity });
+          const actor = Actor.createActor(idlFactory, { agent, canisterId: blockBoltcanisterId });
+
+          setActor(actor);
+        },
+      });
+    }
+  };
+
+  const addr = 'rjjpj-6zdeu-ifzlc-5b7ip-g2uyw-jtpyc-fevjt-tvqcs-dii2o-shr5f-xae'; // Replace with actual Principal ID
+
+
+  const transfer = async () => {
+    if (actor) {
+      const toAccount = {
+        owner: Principal.fromText(addr!),
+        subaccount: [],
+      };
+      const amount = BigInt(10000); // Example amount
+      const result = await actor.transfer({ toAccount, amount });
+      console.log(result);
+    }
+  };
+
+  const fetchAddressData = async () => {
+    if (actor) {
+      // Replace with your canister method to fetch address data
+      const data = await actor.getAddressData(); // Example method
+      setAddressData(data);
+    }
+  };
+
+  const approve = async () => {
+    try {
+      const canisterId = nnsCanisterId;
+      console.log(canisterId, 'demo');
+
+      const identity = await client.getIdentity();
+      const agent = new HttpAgent({ identity: identity });
+
+      const actor = Actor.createActor(idlFactory, { agent, canisterId: blockBoltcanisterId });
+
+
+      const text = 'My first BTC transfer';
+      const memo = Array.from(new TextEncoder().encode(text));
+
+      console.log(actor, 'actor');
+
+
+      const exmple = await actor.icrc2_approve({
+        from: Principal.fromText('rjjpj-6zdeu-ifzlc-5b7ip-g2uyw-jtpyc-fevjt-tvqcs-dii2o-shr5f-xae'),
+        // from: Principal.fromText(
+        //   '3zm3c-qhy2j-5vvce-il3fu-il4ai-wf5fs-bwwjp-dbary-numis-kgxui-rae'
+        // ),
+
+        spender: {
+          owner: Principal.fromText('bza44-ciaaa-aaaan-qlvna-cai'),
+          subaccount: [],
+        },
+        fee: [],
+        memo: [memo],
+        from_subaccount: [],
+        created_at_time: [],
+        expires_at: [],
+        expected_allowance: [],
+        amount: BigInt(10000),
+      });
+    } catch (error: any) {
+      console.error('Error in randomTransfers:', error);
+      setErrorMessage(
+        error.message || 'An error occurred during the transfer.'
+      );
+    }
+  };
+
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="flex flex-row">
+      <Button onClick={login}>Login</Button>
+      <Button onClick={transfer}>Transfer</Button>
+      <Button onClick={fetchAddressData}>Fetch Address Data</Button>
+
+      {addressData && (
+        <div>
+          <h3>Address Data</h3>
+          <pre>{JSON.stringify(addressData, null, 2)}</pre>
         </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      )}
+    </div>
   );
 }
